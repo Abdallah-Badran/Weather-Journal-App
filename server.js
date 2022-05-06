4349193f7d3e57a1c7d598522c208120cdb84223
Abdallah Badran <84018166+Abdallah-Badran@users.noreply.github.com>
2022-05-06 16:48:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,21 @@
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {};
+const express = require('express')
 
-// Require Express to run server and routes
-const express = require('express') ;
+const app = express()
 
-// Start up an instance of app
-const app = express() ;
+let serverData = {}
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser') ;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.static('frontend'))
+app.use(express.json())
 
-// Cors for cross origin allowance
-const cors = require('cors') ;
-app.use(cors()) ;
+app.listen(8000, ()=>{
+    console.log('server is running')
+})
 
-// pointing to the main client-side folder
-app.use(express.static('website'));
+app.post('saveData', (req, res)=>{
+    serverData = req.body
+    res.json({msg : 'done'})
+})
 
-
-// Setup Server with a callback to debug
-app.listen(3000, function() {console.log('Server is set up')}) ;
-
-// post route to recieve and save data
-app.post('/saveToEndPoint', saveToEndPoint) ;
-function saveToEndPoint(req, res)
-{
-    projectData.date = req.body.date,
-    projectData.temperature = req.body.temperature,
-    projectData.feeling = req.body.feeling 
-}
-
-// get route to update UI with the end point data
-app.get('/updateUI', endPointToUI) ;
-function endPointToUI(request, response)
-{
-    response.send(projectData) ;
-}
+app.get('/getData', (req, res)=>{
+    res.json(serverData)
+})
